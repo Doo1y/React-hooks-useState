4e2e72f78cf1d2463a97ef43876f7ce9dd21d478
Samuel Park <samjpark1497@gmail.com>
2022-11-29 22:50:58 +0000

    Update 3 files

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,33 @@
+import './UseState.css';
+import { useState } from 'react';
+
 const UseState = () => {
+  const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState(0);
+  
+  const switchTheme = () => {
+    const buttonEl = document.querySelector('button');
+    if (theme === 'light') {
+      buttonEl.innerText = 'light';
+      return setTheme('dark')
+    }
+    buttonEl.innerText = 'dark';
+    return setTheme('light')
+  };
+  
   return (
-    <div>
+    <div className={`${theme} state`}>
       <h1>UseState Component</h1>
+      <button onClick={switchTheme}>
+        dark
+      </button>
+      <h1>Click Count: {count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
     </div>
   );
 };
